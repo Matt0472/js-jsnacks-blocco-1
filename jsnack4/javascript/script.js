f043a762b574c03b6f6253e5abeff95bd469b125
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-10 17:21:42 +0100

    jsnack4 completato

--- jsnack4/javascript/script.js
+++ jsnack4/javascript/script.js
@@ -2,3 +2,20 @@
 // In un array sono contenuti i nomi degli invitati alla festa del
 // grande Gatsby, chiedi all’utente il suo nome e comunicagli se
 // può partecipare o no alla festa.
+
+var listaInvitati = ['Jacopo', 'Roberta', 'Giulia', 'Nathan', 'Marco'];
+var nomeUtente = prompt('Qual è il tuo nome?');
+
+var nomeUtentePresente = false;
+
+for (var i = 0; i < listaInvitati.length; i++) {
+  var nomeCorrente = listaInvitati[i];
+  if (nomeUtente == nomeCorrente) {
+    nomeUtentePresente = true;
+  }
+}
+if (nomeUtentePresente == true) {
+   alert('Benvenuto al party!');
+ } else {
+   alert('Siamo spiacenti di comunicarti che il tuo nome non è presente nella lista invitati');
+ }
