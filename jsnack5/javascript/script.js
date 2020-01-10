55f446ed2e49d95189c980bf838ac58459a79831
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-10 18:22:35 +0100

    jsnack5 completato

--- jsnack5/javascript/script.js
+++ jsnack5/javascript/script.js
@@ -1,3 +1,13 @@
 // Snack5
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
 // un numero, se è dispari inseriscilo nell’array.
+
+var emptyArray = [];
+
+for (var i = 0; i < 6; i++) {
+  var userNumber = parseInt(prompt('Inserisci un numero'));
+  if (userNumber % 2 != 0) {
+    emptyArray.push(userNumber);
+  }
+}
+console.log(emptyArray);
