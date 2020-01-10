cdf9072e843410291fa9516aa5d0c7db23201ba6
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-10 16:49:23 +0100

    jsnack1 completato

--- jsnack1/javascript/script.js
+++ jsnack1/javascript/script.js
@@ -1,3 +1,14 @@
 // Snack1
 // L’utente inserisce due numeri in successione, con due
 // prompt. Il software stampa il maggiore.
+
+var numero1 = parseInt(prompt('Inserisci il primo numero'));
+var numero2 = parseInt(prompt('Inserisci il secondo numero'));
+
+if (numero1 > numero2) {
+  console.log(numero1);
+} else if (numero1 < numero2) {
+  console.log(numero2);
+} else {
+  console.log('I numeri scelti sono uguali!');
+}
