5d01857d9b601d1a711bec2bf8bc5daa28f257a1
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-13 11:59:15 +0100

    jsnack9 da completare

---
+++ jsnack9/javascript/script.js
@@ -0,0 +1,9 @@
+// Scrivi un generatore di frasi della Fortuna...
+// ad ogni refresh della pagiuna comparirà una frase a caso tra un set di frasi
+// Oggi troverai una cosa dimenticata
+// Ciò che è fatto è fatto
+// Il buon giorno si vede dal mattino
+// Sicuro di aver chiuso il gas?
+// Chi semina vento raccoglie tempesta
+// Il mattino ha l'oro in bocca
+// Tanto va la gatta al lardo che ci lascia lo zampino
